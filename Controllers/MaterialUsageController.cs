80c9c303ada54863ac981a9f984036fd10570fd4
AuliaSabrina02 <aauliasabrina14@gmail.com>
2024-11-30 17:20:22 +0700

    update material usage history on assistant menu

--- Controllers/MaterialUsageController.cs
+++ Controllers/MaterialUsageController.cs
@@ -11,9 +11,9 @@ namespace PBL_IF6_Panasonic.Controllers
            
             var materialUsages = new List<dynamic>
             {
-                new { No = 1, RecipientName = "Aulia", EmployeeId = "EMP123", MaterialName = "Copper", Weight = 100.0, Quantity = 10, Unit = "kg", Line = "Line 1" },
-                new { No = 2, RecipientName = "Sabrina", EmployeeId = "EMP124", MaterialName = "Plastic", Weight = 200.0, Quantity = 20, Unit = "kg", Line = "Line 2" },
-                new { No = 3, RecipientName = "Aulia Sabrina", EmployeeId = "EMP125", MaterialName = "Steel", Weight = 500.0, Quantity = 50, Unit = "kg", Line = "Line 3" },
+                new { No = 1, RecipientName = "Aulia",  PartNumber ="A1002", MaterialName = "Copper",  Quantity = "10 Kg",  DetailUnit="1 Spool = 100kg ", Unit = "Spool", Line = "Line 1" },
+                new { No = 2, RecipientName = "Sabrina", PartNumber ="A1003", MaterialName = "Plastic",  Quantity = "20 Kg" , DetailUnit="1 Spool = 100kg ",  Unit = "Spool", Line = "Line 2" },
+                new { No = 3, RecipientName = "Aulia Sabrina",  PartNumber ="A1004", MaterialName = "Steel", Quantity = "50 Kg" , DetailUnit="1 Spool = 100kg ",  Unit = "Spool", Line = "Line 3" },
           
             };
 
