49d76f34bf94222165496aee527e47ab68c5a7c1
farhannajedi <najedifarhan.com>
2024-12-16 13:12:23 +0700

    tambah data table

---
+++ Controllers/ManagePrepRoomController.cs
@@ -0,0 +1,23 @@
+// using System.Collections.Generic;
+// using Microsoft.AspNetCore.Mvc;
+
+// namespace PBL_IF6_Panasonic.Controllers
+// {
+//     public class ManagePrepRoomController : Controller
+//     {
+//         public IActionResult Index()
+//         {
+
+//             private static List <ManagePrepRoom> materials = new List<ManagePrepRoom> 
+//             {
+//                 new ManagePrepRoom { No = 1, MaterialName = "Cement", MaterialImage = }
+//             }
+//         }
+
+//         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+//         public IActionResult Error()
+//         {
+//             return View("Index");
+//         }
+//     }
+// }
